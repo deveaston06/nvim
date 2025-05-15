cb708f75e2fb60d2b011d26f0ff58b679a5a836c
Dev Easton <deveaston06@gmail.com>
2025-05-15 19:06:51 +0800

    customize theme and add other plugins + keymap

--- init.lua
+++ init.lua
@@ -1,2 +1,23 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.g.material_style = "palenight"
+require("material").setup({
+  styles = { -- Give comments style such as bold, italic, underline etc.
+    comments = { italic = false },
+  },
+  high_visibility = {
+    lighter = false, -- Enable higher contrast text for lighter style
+    darker = false, -- Enable higher contrast text for darker style
+  },
+  lualine_style = "stealth", -- Lualine style ( can be 'stealth' or 'default' )
+  disable = {
+    colored_cursor = false, -- Disable the colored cursor
+    borders = false, -- Disable borders between vertically split windows
+    background = true, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
+    term_colors = true, -- Prevent the theme from setting terminal colors
+    eob_lines = false, -- Hide the end-of-buffer lines
+  },
+})
+
+vim.cmd("colorscheme material")
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- default map options
+local opts = { noremap = true, silent = true }
+
+-- remove ^M after <p> from Windows System Clipboard
+vim.keymap.set("n", "gm", function()
+  vim.cmd(":%s/\r//g")
+end, opts)
