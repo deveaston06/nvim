af3a70913ab7b4ffd799d628f55eda0d556a624a
deveaston06 <deveaston06@gmail.com>
2025-05-29 17:25:23 +0800

    added Octo plugin, layzvim update created lazy-lock.json

--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -158,4 +158,14 @@ return {
       },
     },
   },
+
+  -- github cli must be installed to use octo.nvim
+  {
+    "pwntester/octo.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("octo").setup()
+    end,
+    cmd = { "Octo" },
+  },
 }
